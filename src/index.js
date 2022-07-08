a926cc0aa3f5662ea3af896b58ea7a8b99cc327c
Lane <Kewllane@aol.com>
2022-07-07 21:04:00 -0500

    Thursday Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const cohortName = '2204-FTB-ET-WEB-PT';
+const APIURL = `https://strangers-things.herokuapp.com/api/${cohortName}`;
+
 const App = () => {
-    
+    const [posts, setPosts] = useState([]);
+    console.log(posts)
+
+    useEffect(() => {
+        const fetchAllPosts = async () => {
+            const response = await fetch (`${APIURL}/posts`);
+            const data = await response.json();
+            setPosts(data.data.posts);
+        }
+        fetchAllPosts();
+    }, [])
+
+    return <div className="app">
+        <header className ="header">
+            <h1>Strangers Things</h1>
+        </header>
+        {
+            posts.map(post => <div key={post._id}>
+                <h3>{post.title}</h3>
+                <div>{post.description}</div>
+            </div>)
+        }
+    </div>
 }
+
+// const app = document.getElementById('app')
+ReactDOM.render(<App />, document.getElementById("app"));
